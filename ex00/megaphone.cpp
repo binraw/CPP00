1876fa5e1d1334bbe3d421a5779e30f0348165d9
Robin Truvelot <rtruvelo@z3r3p1.42lyon.fr>
2024-09-24 10:26:29 +0200

    fix

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -7,14 +7,9 @@ int main(int argc, char **argv)
     for (int y = 1; y < argc; y++)
 	{
         size_t length = std::strlen(argv[y]);
-        for (size_t i = 0; i < length; i++)
-		{
-            if (argv[y][i] >= 'a' && argv[y][i] <= 'z') 
-			{
-                argv[y][i] -= 32;
-            }
-        }
-        std::cout << argv[y] << " ";
+        for (size_t i = 0; i < length; ++i)
+                std::cout << (char)std::toupper(argv[y][i]);
+        std::cout << " ";
     }
 
     std::cout << std::endl;
